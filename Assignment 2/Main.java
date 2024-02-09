269ab3d7744731fc245b202c02c445dc86ff2d61
Jerry <70291890+dev88jerry@users.noreply.github.com>
2024-02-09 16:24:15 -0500

    Create Main.java

---
+++ Assignment 2/Main.java
@@ -0,0 +1,86 @@
+package Assignment;
+
+/**
+ * Description:
+ * This program is used to test the Library and Book classes by using two libraries that will
+ * contain collections of books. It will rent and return books.
+ * @author Gregory
+ *
+ */
+
+public class Main {
+	
+	
+	public static void main (String[] args){
+			
+		// Create two libraries
+	
+		Library firstLibrary = new Library("120 Queen St.");
+	
+		Library secondLibrary = new Library("228 College St.");
+	
+		// Add four books to the first library
+	
+		firstLibrary.addBook(new Book("The Da Vinci Code"));
+	
+		firstLibrary.addBook(new Book("Le Petit Prince"));
+	
+		firstLibrary.addBook(new Book("A Tale of Two Cities")); 
+		
+		firstLibrary.addBook(new Book("The Lord of the Rings"));
+	
+		// Print opening hours and the addresses
+	
+		System.out.println("Library hours:");
+	
+		Library.printOpeningHours();
+	
+		System.out.println();
+	
+		 
+	
+		System.out.println("Library addresses:"); 
+		
+		firstLibrary.printAddress();
+	
+		secondLibrary.printAddress();
+	
+		System.out.println();
+	
+		 
+	
+		// Try to borrow The Lords of the Rings from both libraries
+	
+		System.out.println("Borrowing The Lord of the Rings:");
+		firstLibrary.borrowBook("The Lord of the Rings"); 
+		firstLibrary.borrowBook("The Lord of the Rings"); 
+		secondLibrary.borrowBook("The Lord of the Rings"); 
+		System.out.println();
+	
+		// Print the titles of all available books from both libraries
+	
+		System.out.println("Books available in the first library:"); 
+		firstLibrary.printAvailableBooks();
+	
+		System.out.println();
+	
+		System.out.println("Books available in the second library:"); 
+		secondLibrary.printAvailableBooks();
+	
+		System.out.println();
+	
+		// Return The Lords of the Rings to the first library
+	
+		System.out.println("Returning The Lord of the Rings:"); 
+		firstLibrary.returnBook("The Lord of the Rings");
+	
+		System.out.println();
+	
+		// Print the titles of available from the first library
+	
+		System.out.println("Books available in the first library:");
+	
+		firstLibrary.printAvailableBooks();
+	}
+	
+}
