694680eb99f7b26926519b6fa157b79ccf13e5d6
Jerry <70291890+dev88jerry@users.noreply.github.com>
2024-02-09 17:46:15 -0500

    Create Course.java

---
+++ Lab 9/Course.java
@@ -0,0 +1,19 @@
+
+public class Course {
+	private String name = null;
+	private Prof prof = null;
+
+	public Course(String aName, Prof aProf) {
+		super();
+		name = aName;
+		prof = aProf;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	Prof getProf() {
+		return prof;
+	}
+}
